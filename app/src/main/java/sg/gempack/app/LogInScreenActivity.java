f64ae8491bd781c18533835a94e021b8610a14a0
Xander Wang <xdrawks@gmail.com>
2016-01-23 18:36:49 +0800

    Backend setup and login

--- app/src/main/java/sg/gempack/app/LogInScreenActivity.java
+++ app/src/main/java/sg/gempack/app/LogInScreenActivity.java
@@ -1,10 +1,24 @@
 package sg.gempack.app;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseFacebookUtils;
+import com.parse.ParseUser;
+
+import java.util.Arrays;
+import java.util.List;
+
+import sg.gempack.app.Classes.GempackUser;
+import sg.gempack.app.Utilities.PushNotificationManagement;
 
 
 public class LogInScreenActivity extends AppCompatActivity {
@@ -19,7 +33,62 @@ public class LogInScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_log_in_screen);
 
 
+
     }
+
+
+    private void LogInFunction (){
+        final Dialog progressDialog = ProgressDialog.show(this, "", "Logging in...", true);
+
+        //List of permissions you need form user's facebook account.
+        List<String> permissions = Arrays.asList("public_profile", "email");
+
+        //documentation: https://parse.com/docs/android/api/com/parse/LogInCallback.html
+        ParseFacebookUtils.logInWithReadPermissionsInBackground(this, permissions, new LogInCallback() {
+            @Override
+            public void done(final ParseUser currentUser, ParseException e) {
+
+                GempackApplication.getMainGempackUser().saveFacebookInfoToParse(LogInScreenActivity.this, new GempackUser.getFacebookInfoCallback() {
+                    @Override
+                    public void onSaveComplete() {
+
+                    }
+
+                    @Override
+                    public void noNeedToSave() {
+
+                    }
+
+                    @Override
+                    public void somethingWentWrong() {
+
+                    }
+                });
+
+                //TODO: Load main activity
+                progressDialog.dismiss();
+            }
+        });
+    }
+
+
+    public static void logOutFunction (final Context context){
+        PushNotificationManagement push = new PushNotificationManagement();
+        push.uninstallPushFromParseInstallation(new PushNotificationManagement.onSettingPushInstallationCallback() {
+            @Override
+            public void onSetupComplete() {
+                ParseUser.logOut();
+                openLogInActivity(context);
+            }
+
+            @Override
+            public void somethingWentWrong() {
+                Toast.makeText(context, "Failed to log out. Please check your internet connection and try again.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 
 
 
---
+++ app/src/main/java/sg/gempack/app/Utilities/ParseExceptionHandler.java
@@ -0,0 +1,89 @@
+package sg.gempack.app.Utilities;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+
+import com.parse.ParseException;
+
+/**
+ * Handles Parse Exceptions
+ *
+ * @Author Wang Xien Dong
+ */
+
+public class ParseExceptionHandler {
+
+
+        public ParseExceptionHandler (Context context){
+            mContext = context;
+        }
+
+        Context mContext;
+
+        public interface ExceptionCallback{
+
+            void retryLastStep();
+            void abortLastStep();
+            void ranSuccessfully();
+            void finallyDo();
+        }
+
+        public void processPotentialParseExceptions(ParseException e, String userAction, final ExceptionCallback callback) {
+
+            // "userAction" --> Something went wrong while... INSERT TEXT HERE ...Please try again.
+
+
+            if (e == null) {
+                callback.ranSuccessfully();
+            } else {
+                Log.e("ParseException", "Error Code: " + e.getCode() + " Error Message: " + e.getMessage());
+                e.printStackTrace();
+
+                switch (e.getCode()) {
+
+                    case (ParseException.CONNECTION_FAILED):
+
+                        if (e.getMessage().equals("bad protocol")) {
+                            callback.retryLastStep();
+                            break;
+                        }
+
+                    case (ParseException.TIMEOUT):
+
+
+                    case (ParseException.EXCEEDED_QUOTA):
+
+
+                    default:
+                        //TODO: REPORT THIS ERROR
+
+                        new AlertDialog.Builder(mContext)
+                                .setTitle("Something Went Wrong")
+                                .setMessage("Something went wrong while " + userAction + " . Please try again.\n" + e.getMessage())
+                                .setPositiveButton("TRY AGAIN", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        callback.retryLastStep();
+                                    }
+                                })
+                                .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        callback.abortLastStep();
+                                    }
+                                })
+                                .setCancelable(false)
+                                .create().show();
+
+                        break;
+
+                }
+            }
+
+            callback.finallyDo();
+
+        }
+
+}
