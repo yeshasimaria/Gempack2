8962d5be71a004c564d7c0ce280dbd90a8fc44b8
Yesha Simaria <yesha.simaria@u.nus.edu>
2016-01-24 00:29:45 +0800

    packCard edited

--- app/src/main/java/sg/gempack/app/Cards/GempackPackCard.java
+++ app/src/main/java/sg/gempack/app/Cards/GempackPackCard.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.v7.widget.CardView;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import sg.gempack.app.Classes.GempackPack;
 import sg.gempack.app.R;
@@ -12,7 +13,7 @@ import sg.gempack.app.R;
  * Created by Yesha on 1/23/2016.
  */
 public class GempackPackCard extends CardView {
-
+    TextView remainingAmount, endDate, vendor, collectionPoint;
     Context context;
     GempackPack gempackPack;
 
@@ -34,6 +35,10 @@ public class GempackPackCard extends CardView {
     private void init(Context context) {
         this.context = context;
         LayoutInflater.from(getContext()).inflate(R.layout.card_gempackpack, this, true);
+        remainingAmount = (TextView) findViewById(R.id.remaining_amount);
+        endDate = (TextView) findViewById(R.id.end_date);
+        vendor = (TextView) findViewById(R.id.vendor_name);
+        collectionPoint = (TextView) findViewById(R.id.collection_point);
     }
 
     public void update(GempackPack gempackPack) {
@@ -42,7 +47,11 @@ public class GempackPackCard extends CardView {
     }
 
     private void update() {
-
+        //TODO
+        //remainingAmount.setText();
+        //endDate.setText();
+        //vendor.setText();
+        collectionPoint.setText(gempackPack.getCollectionPoint());
     }
 
 
