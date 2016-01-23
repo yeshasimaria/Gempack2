8c896e2d6351ba132574038bf1919d498c7fbc5c
Yesha Simaria <yesha.simaria@u.nus.edu>
2016-01-23 21:58:16 +0800

    yesha's progress

---
+++ app/src/main/java/sg/gempack/app/Adapters/GempackPackAdapter.java
@@ -0,0 +1,87 @@
+package sg.gempack.app.Adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import sg.gempack.app.Cards.GempackPackCard;
+import sg.gempack.app.Classes.GempackPack;
+import sg.gempack.app.R;
+
+/**
+ * Created by Yesha on 1/23/2016.
+ */
+public class GempackPackAdapter extends RecyclerView.Adapter<GempackPackAdapter.GempackPackViewHolder> implements View.OnClickListener{
+
+        ArrayList<GempackPack> mGempackPackSet;
+        Context context;
+
+        public GempackPackAdapter(Context context,ArrayList<GempackPack>GempackPackset){
+        this.context=context;
+        mGempackPackSet=GempackPackset;
+        }
+
+        @Override
+        public GempackPackViewHolder onCreateViewHolder(ViewGroup viewGroup,int i){
+        View v= LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recycler_view_item_gempackpack,viewGroup,false);
+        GempackPackViewHolder pvh=new GempackPackViewHolder(v,context);
+        return pvh;
+        }
+
+        @Override
+        public void onBindViewHolder(GempackPackViewHolder gempackPackViewHolder,int i){
+        gempackPackViewHolder.cv.update(mGempackPackSet.get(i));
+        }
+
+        @Override
+        public int getItemCount(){
+        if(mGempackPackSet!=null)
+        return mGempackPackSet.size();
+        else return 0;
+        }
+
+public void updateList(ArrayList<GempackPack>newlist){
+        mGempackPackSet.clear();
+        mGempackPackSet.addAll(newlist);
+        this.notifyDataSetChanged();
+        }
+
+@Override
+public void onAttachedToRecyclerView(RecyclerView recyclerView){
+        super.onAttachedToRecyclerView(recyclerView);
+        }
+
+@Override
+public void onClick(View v){
+
+        }
+
+public static class GempackPackViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    GempackPackCard cv;
+    Context ctx;
+
+    GempackPackViewHolder(View itemView, Context context) {
+        super(itemView);
+        ctx = context;
+        cv = (GempackPackCard) itemView.findViewById(R.id.gempackpack_card_item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            //case R.id.paid:
+            //String[] params = new String[1];
+            //break;
+            //case R.id.delete:
+            //String[] params1 = new String[1];
+            default:
+                break;
+        }
+    }
+}
+}
+
